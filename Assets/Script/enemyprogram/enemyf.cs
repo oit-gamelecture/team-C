09e5b25c3b81ee6cb10a55115418cdef4abc0540
e1q24077 <e1q24077@oit.ac.jp>
2024-08-22 14:12:38 +0900

    スクリプトに移動　藤田

--- Assets/Script/enemyprogram/enemyf.cs
+++ Assets/Script/enemyprogram/enemyf.cs
@@ -6,6 +6,10 @@ public class enemyf : MonoBehaviour
 {
     // Start is called before the first frame update
     private float speed = 0.01f;
+    private float downspeed = 0.1f;
+    public bool down;
+    public bool enemystop;
+    public bool destroy;
     void Start()
     {
         
@@ -14,6 +18,42 @@ public class enemyf : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f,0f,speed);
+        if(destroy == true)
+        {
+            Destroy (this.gameObject);
+        }
+        if(down == true)
+        {
+            StartCoroutine("StopHoge");
+        }
+        else if(down == false && enemystop == true)
+        {
+            transform.Translate(0f,0f,0f);
+        }
+        else
+        {
+            transform.Translate(0f,0f,-speed);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name=="m01_blazer_000_h")
+        {
+            down = true;
+        }
+    }
+    IEnumerator StopHoge()
+    {
+        transform.Translate(0f,0f,downspeed);
+        yield return new WaitForSeconds(0.3f);
+        down = false;
+        enemystop = true;
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "DestroyArea" )
+        {
+            destroy = true;
+        }
     }
 }
